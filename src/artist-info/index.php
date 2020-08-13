e8c4092b8d08745e49a7d4d8700f7768373cbad3
Geoff Kendal <gkendal@williamhill.co.uk>
2020-08-13 19:49:03 +0100

    removes

--- src/artist-info/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  header("Location: https://docs.google.com/forms/d/e/1FAIpQLSf3lpuKHfIMGnZKxMHths5NHUSGxHH2WBactfz0AzQAVmTk3w/viewform");
-
-?>
--- src/group_login_do.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("config.php");
-
-$_SESSION[user] = $_GET[guid];
-header("Location: tickets.php");
-
-?>
-
