a73dcaf4eda1e90c481324fbed675ac2317a25d6
Geoff Kendal <gkendal@williamhill.co.uk>
2020-09-06 17:31:55 +0100

    Working Lambda call

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
 import React, { Component } from "react";
-import Groups from "./Groups";
+import Table from "./Table";
 import Form from "./Form";
 
 class App extends Component {
   state = {
+    error: null,
+    isLoaded: false,
     characters: [],
-    groups: [],
+    items: [],
   };
 
-  
-
   componentDidMount() {
-    var data = {
-      userID: "geoff"
-    }
-
-    fetch('https://0ikcn1wb5l.execute-api.eu-west-1.amazonaws.com/dev/groups/list', {
+    const requestOptions = {
       method: 'POST',
-      headers: {
-        'Content-Type': 'applications/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then((response) => response.json())
-    .then(booksList => {
-        this.setState({ groups: booksList });
-    });
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userID: 'geoff' })
+    };
+
+    fetch("https://0ikcn1wb5l.execute-api.eu-west-1.amazonaws.com/dev/groups/list", requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.message
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
-    const { characters } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello, MyApp!</h1>
-        <Groups data={this.state.groups}/>
-      </div>
-    );
+    const { characters, error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+
+      return (
+        <div className="App">
+          <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.id} {item.adults}
+            </li>
+          ))}
+        </ul>
+          <h1>Hello, MyApp!</h1>
+          <Table
+            characterData={characters}
+            removeCharacter={this.removeCharacter}
+          />
+          <Form handleSubmit={this.handleSubmit} />
+        </div>
+      );
+
+    }
   }
 
   removeCharacter = (index) => {
