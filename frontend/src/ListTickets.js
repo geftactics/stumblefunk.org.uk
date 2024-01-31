87fc82da6b70b14b57314e1af09471a671580735
Geoff Kendal <gkendal@williamhill.co.uk>
2024-01-31 13:38:45 +0000

    search fixed

--- frontend/src/ListTickets.js
+++ frontend/src/ListTickets.js
@@ -46,6 +46,7 @@ const ListTickets = ({ groupCode, ticketType }) => {
               ...prevTickets,
               [group.group_name]: selectedTickets,
             }));
+
           } else {
             console.error('Error fetching tickets data:', ticketsResponse.statusText);
           }
@@ -61,15 +62,23 @@ const ListTickets = ({ groupCode, ticketType }) => {
   }, [groups, groupCode, ticketType]);
 
   const filterTickets = (ticket, groupName) => {
-    const searchTermLowerCase = searchTerm.toLowerCase();
-    return (
-      ticket.first_name.toLowerCase().includes(searchTermLowerCase) ||
-      ticket.last_name.toLowerCase().includes(searchTermLowerCase) ||
-      ticket.involvement.toLowerCase().includes(searchTermLowerCase) ||
-      ticket.mobile_phone.toLowerCase().includes(searchTermLowerCase) ||
-      ticket.email.toLowerCase().includes(searchTermLowerCase) ||
-      groupName.toLowerCase().includes(searchTermLowerCase)
-    );
+    const ticketFields = {
+      adult: ['first_name', 'last_name', 'involvement', 'mobile_phone', 'email'],
+      child: ['first_name', 'last_name', 'ticket_id', 'child_age', 'mobile_phone'],
+      vehicle: ['driver_id', 'vehicle_reg', 'vehicle_size', 'vehicle_parking', 'mobile_phone'],
+    };
+  
+    const groupFields = ['group_name']; // Add group_name to the fields to search
+  
+    const fieldsToSearch = ticketFields[ticketType].concat(groupFields);
+    const searchableText = fieldsToSearch.map(field => {
+      if (field === 'group_name') {
+        return groupName; // Use group name for the 'group_name' field
+      }
+      return ticket[field];
+    }).join(' ');
+  
+    return searchableText.toLowerCase().includes(searchTerm.toLowerCase());
   };
 
   return (
@@ -80,34 +89,70 @@ const ListTickets = ({ groupCode, ticketType }) => {
         id="myInput"
         type="text"
         placeholder="Search..."
-        value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <br/>
+      /><br/>
       <table className="table table-striped">
         <thead>
           <tr>
             <th>Collective</th>
-            <th>Name</th>
-            <th>Involvement</th>
-            <th>Mobile Phone</th>
-            <th>Email</th>
+            {ticketType === 'adult' && (
+              <>
+                <th>Name</th>
+                <th>Involvement</th>
+                <th>Mobile Phone</th>
+                <th>Email</th>
+              </>
+            )}
+            {ticketType === 'child' && (
+              <>
+                <th>Name</th>
+                <th>Child Age</th>
+                <th>Parent</th>
+                <th>Mobile Phone</th>
+              </>
+            )}
+            {ticketType === 'vehicle' && (
+              <>
+                <th>Driver</th>
+                <th>Vehicle Type</th>
+                <th>Vehicle Parking</th>
+                <th>Mobile Phone</th>
+              </>
+            )}
           </tr>
         </thead>
         <tbody id="myTable">
           {Object.entries(tickets).map(([groupName, groupTickets]) => (
             <React.Fragment key={groupName}>
-              {groupTickets
-                .filter(ticket => filterTickets(ticket, groupName))
-                .map((ticket, index) => (
-                  <tr key={`${groupName}-${index}`}>
-                    <td>{groupName}</td>
-                    <td>{ticket.first_name} {ticket.last_name}</td>
-                    <td>{ticket.involvement}</td>
-                    <td>{ticket.mobile_phone}</td>
-                    <td>{ticket.email}</td>
-                  </tr>
-                ))}
+              {groupTickets.filter(ticket => filterTickets(ticket, groupName)).map(ticket => (
+                <tr key={ticket.ticket_id}>
+                  <td>{groupName}</td>
+                  {ticketType === 'adult' && (
+                    <>
+                      <td>{`${ticket.first_name} ${ticket.last_name}`}</td>
+                      <td>{ticket.involvement}</td>
+                      <td>{ticket.mobile_phone}</td>
+                      <td>{ticket.email}</td>
+                    </>
+                  )}
+                  {ticketType === 'child' && (
+                    <>
+                      <td>{`${ticket.first_name} ${ticket.last_name}`}</td>
+                      <td>{ticket.child_age}</td>
+                      <td>{ticket.parent_id}</td>
+                      <td>{ticket.mobile_phone}</td>
+                    </>
+                  )}
+                  {ticketType === 'vehicle' && (
+                    <>
+                      <td>{`${ticket.driver_id}`}</td>
+                      <td>{ticket.vehicle_size}</td>
+                      <td>{ticket.vehicle_parking}</td>
+                      <td>{ticket.mobile_phone}</td>
+                    </>
+                  )}
+                </tr>
+              ))}
             </React.Fragment>
           ))}
         </tbody>
